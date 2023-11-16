a29054bc4f3ea01a2763fa07cae807d610867ed5
aytajabdullayeva <abdullayevaaytaj94@gmail.com>
2023-11-15 23:52:34 -0500

    My first selenium commit

--- src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -1,6 +1,6 @@
 package com.loop.test.day1_selenium_intro;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
